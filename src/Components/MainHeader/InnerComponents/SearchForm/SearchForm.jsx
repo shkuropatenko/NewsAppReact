6964401510aa8a7a4fc60ffc2baee5f3fc51981c
Dmitriy Sh <dmitriy.sh@darly.solutions>
2021-06-18 12:27:06 +0300

    added search for app

--- src/Components/MainHeader/InnerComponents/SearchForm/SearchForm.jsx
+++ src/Components/MainHeader/InnerComponents/SearchForm/SearchForm.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
+import { useContext } from "react";
+import context from "../../../../context";
+
 import styles from "./SearchFormStyle.module.scss";
 
 const SearchForm = () => {
+  const { initPosts, setFilteredPosts } = useContext(context);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (search) {
+      const filteredArr = initPosts.filter((post) =>
+        post.webTitle.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredPosts(filteredArr);
+    } else {
+      setFilteredPosts([]);
+    }
+  };
+
+  const clearSearch = () => {
+    setFilteredPosts(initPosts);
+    setSearch("");
+  };
 
   return (
     <form className={styles.search_form}>
-      <button type="submit">
+      <button onClick={handleSearch} type="submit">
         <i className="icon-search-icon"></i>
       </button>
-      <input type="search" placeholder="Type something to start search" />
+      <button onClick={clearSearch} className={search ? `${styles.clear_btn} ${styles.change}` : `${styles.clear_btn}`}>
+        <span></span>
+        <span></span>
+      </button>
+      <input onChange={handleChange} type="search" placeholder="Type something to start search" />
     </form>
   );
 }
